155648d81274bfd0d25253b7deed8e963a348721
mb4ndeira <mbandeira.cs@gmail.com>
2023-08-13 16:27:24 -0300

    Initialize repository

---
+++ src/app/layout.tsx
@@ -0,0 +1,27 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+
+const poppins = Poppins({
+  weight: ["400", "600"],
+  subsets: ["latin"],
+  display: "swap",
+  preload: false,
+});
+
+export const metadata: Metadata = {
+  title: "Painel e-COO",
+  description: "Administração de cooperativa",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={poppins.className}>{children}</body>
+    </html>
+  );
+}

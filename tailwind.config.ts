6a2c80bf71ed286937641ea916a0e26197010566
mb4ndeira <mbandeira.cs@gmail.com>
2023-08-18 09:31:58 -0300

    feat: add products page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,24 +7,26 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    colors: {
-      "slate-gray": "#2F4A4D",
-      "rain-forest": "#00735E",
-      eucalyptus: "#43DBAB",
-      caramel: "#FFDD92",
-      "blanched-almoad": "#FFEDCC",
-      platinum: "#EAE7E3",
-      "ghost-white": {
-        base: "#FAFCFF",
-        100: "#FAFCFF",
-        200: "#D8DADD", // this is intentional
-        300: "#D8DADD",
-        400: "#B7B8BA",
-        500: "#959698",
-        600: "#737475",
-        700: "#515253",
-        800: "#303030",
-        900: "#0E0E0E",
+    extend: {
+      colors: {
+        "slate-gray": "#2F4A4D",
+        "rain-forest": "#00735E",
+        eucalyptus: "#43DBAB",
+        caramel: "#FFDD92",
+        "blanched-almoad": "#FFEDCC",
+        platinum: "#EAE7E3",
+        "ghost-white": {
+          base: "#FAFCFF",
+          100: "#FAFCFF",
+          200: "#D8DADD", // this is intentional
+          300: "#D8DADD",
+          400: "#B7B8BA",
+          500: "#959698",
+          600: "#737475",
+          700: "#515253",
+          800: "#303030",
+          900: "#0E0E0E",
+        },
       },
     },
   },

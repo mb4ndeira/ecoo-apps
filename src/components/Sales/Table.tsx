144afa5db4ca3f62755480793caf0a0095a29ea0
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-10-04 17:55:01 -0300

    feat: add fake data from aarray to fill the table

--- src/components/Sales/Table.tsx
+++ src/components/Sales/Table.tsx
@@ -1,14 +1,18 @@
 "use client";
 import { PureComponent } from "react";
 import { HiOutlinePencil, HiOutlineFilter } from "react-icons/hi";
+import fakeData from "./data/fakedata";
 
 export class Table extends PureComponent {
   render() {
+    const maxRows = 8;
+    const dataToDisplay = fakeData.slice(0, maxRows);
+
     return (
       <div>
         <div className="relative ml-auto flex items-center">
           <input
-            className="border border-primary rounded-md h-12 p-4 pr-10 text-base ml-auto -mb-4"
+            className="border border-primary rounded-md h-12 p-4 pr-10 text-base ml-auto -mb-4 inter-font w-80"
             type="text"
             placeholder="Procurar"
           />
@@ -20,7 +24,7 @@ export class Table extends PureComponent {
           <span className="text-base font-semibold mb-2 ">Últimas vendas</span>
         </div>
         <div className="flex">
-          <table className="bg-white text-primary text-left leading-7 font-inter w-full table-fixed rounded-lg">
+          <table className="bg-white text-primary text-left leading-7 inter-font w-full table-fixed rounded-lg">
             <thead>
               <tr>
                 <th className="border-b border-primary p-2">ID da venda</th>
@@ -31,150 +35,39 @@ export class Table extends PureComponent {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td className="border-b border-primary p-2 md-mobile:bg-primary md-mobile:text-white">
-                  4985278501
-                </td>
-                <td className="border-b border-primary p-2">R$ 30,45</td>
-                <td className="border-b border-primary p-2">15/08/2023</td>
-                <td className="border-b border-primary p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="border-b border-primary p-2 flex items-center">
-                  <button className="rounded-3xl bg-primary text-white text-sm h-9 w-24 font-semibold">
-                    Pendente
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border-b border-primary p-2 md-mobile:bg-primary md-mobile:text-white">
-                  4985278501
-                </td>
-                <td className="border-b border-primary p-2">R$ 30,45</td>
-                <td className="border-b border-primary p-2">15/08/2023</td>
-                <td className="border-b border-primary p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="border-b border-primary p-2 flex items-center">
-                  <button className="rounded-3xl bg-primary text-white text-sm h-9 w-24 font-semibold">
-                    Pendente
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border-b border-primary p-2 md-mobile:bg-primary md-mobile:text-white">
-                  4985278501
-                </td>
-                <td className="border-b border-primary p-2">R$ 30,45</td>
-                <td className="border-b border-primary p-2">15/08/2023</td>
-                <td className="border-b border-primary p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="border-b border-primary p-2 flex items-center">
-                  <button className="rounded-3xl bg-primary text-white text-sm h-9 w-24 font-semibold">
-                    Pendente
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border-b border-primary p-2 md-mobile:bg-primary md-mobile:text-white">
-                  4985278501
-                </td>
-                <td className="border-b border-primary p-2">R$ 30,45</td>
-                <td className="border-b border-primary p-2">15/08/2023</td>
-                <td className="border-b border-primary p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="border-b border-primary p-2 flex items-center">
-                  <button className="rounded-3xl bg-primary text-white text-sm h-9 w-24 font-semibold">
-                    Pendente
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border-b border-primary p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                  4985278501
-                </td>
-                <td className="border-b border-primary p-2">R$ 30,45</td>
-                <td className="border-b border-primary p-2">15/08/2023</td>
-                <td className="border-b border-primary p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="border-b border-primary p-2 flex items-center">
-                  <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                    Concluído
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border-b border-primary p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                  4985278501
-                </td>
-                <td className="border-b border-primary p-2">R$ 30,45</td>
-                <td className="border-b border-primary p-2">15/08/2023</td>
-                <td className="border-b border-primary p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="border-b border-primary p-2 flex items-center">
-                  <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                    Concluído
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
-              <tr>
-                <td className="border-b border-primary p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                  4985278501
-                </td>
-                <td className="border-b border-primary p-2">R$ 30,45</td>
-                <td className="border-b border-primary p-2">15/08/2023</td>
-                <td className="border-b border-primary p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="border-b border-primary p-2 flex items-center">
-                  <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                    Concluído
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
-              <tr className="border-none">
-                <td className="p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                  4985278501
-                </td>
-                <td className="p-2">R$ 30,45</td>
-                <td className="p-2">15/08/2023</td>
-                <td className="p-2">
-                  2kg tomate, 1kg batata branca, 1kg cenoura
-                </td>
-                <td className="p-2 flex items-center">
-                  <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                    Concluído
-                  </button>
-                  <button className="ml-auto mr-2  text-xl">
-                    <HiOutlinePencil />
-                  </button>
-                </td>
-              </tr>
+              {dataToDisplay.map((item, index) => (
+                <tr
+                  key={item.id}
+                  className={`${
+                    index === dataToDisplay.length - 1
+                      ? "border-t-0 border-b-0"
+                      : "border-b border-primary"
+                  }`}
+                >
+                  <td className="p-2">{item.id}</td>
+                  <td className="p-2">{item.valor}</td>
+                  <td className="p-2">{item.dataVenda}</td>
+                  <td className="p-2">
+                    {item.descricao.length > 40
+                      ? `${item.descricao.substring(0, 40)}...`
+                      : item.descricao}
+                  </td>
+                  <td className="p-2 flex items-center">
+                    <button
+                      className={`rounded-3xl ${
+                        item.situacao === "Pendente"
+                          ? "bg-primary text-white"
+                          : "bg-secondary text-primary"
+                      } text-sm h-9 w-24 font-semibold`}
+                    >
+                      {item.situacao}
+                    </button>
+                    <button className="ml-auto mr-2  text-xl">
+                      <HiOutlinePencil />
+                    </button>
+                  </td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>

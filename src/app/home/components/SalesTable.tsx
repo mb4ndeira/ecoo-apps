c76a656c2cbedcd95e7a79a15edfae1ec36f5861
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-10-10 15:41:19 -0300

    feat: added universal table component

--- src/app/home/components/SalesTable.tsx
+++ src/app/home/components/SalesTable.tsx
@@ -1,99 +1,33 @@
-export function SalesTable() {
-  return (
-    <div className="flex flex-col items-start max-w-[560px]">
-      <h3 className=" text-base font-semibold ml-2 mb-2 sm-mobile:text-sm">
-        Últimas vendas
-      </h3>
-      <div className="flex sm-mobile:w-[16rem] md-mobile:max-w-[20rem] max-w-[70rem]  sidebar-bp:max-w-[30rem] sm-table:max-w-[22rem] xs-table:max-w-[18rem] overflow-x-auto overflow-y-hidden ">
-        <table className="bg-white text-primary text-left leading-7 font-inter table-fixed rounded-lg w-full h-fit">
-          <thead>
-            <tr>
-              <th className="w-28 border-b border-primary p-2">ID da venda</th>
-              <th className="w-24 border-b border-primary p-2">Valor</th>
-              <th className="w-28 border-b border-primary p-2">Situação</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td className="border-b border-primary p-2 md-mobile:bg-primary md-mobile:text-white">
-                4985278501
-              </td>
-              <td className="border-b border-primary p-2">R$ 30,45</td>
-              <td className="border-b border-primary p-2">
-                <button className="rounded-3xl bg-primary text-white text-sm h-9 w-24 font-semibold">
-                  Pendente
-                </button>
-              </td>
-            </tr>
-            <tr className="border-none">
-              <td className="p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                4985278501
-              </td>
-              <td className="p-2">R$ 30,45</td>
-              <td className="p-2">
-                <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                  Concluído
-                </button>
-              </td>
-            </tr>
-            <tr className="border-none">
-              <td className="p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                4985278501
-              </td>
-              <td className="p-2">R$ 30,45</td>
-              <td className="p-2">
-                <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                  Concluído
-                </button>
-              </td>
-            </tr>
-            <tr className="border-none">
-              <td className="p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                4985278501
-              </td>
-              <td className="p-2">R$ 30,45</td>
-              <td className="p-2">
-                <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                  Concluído
-                </button>
-              </td>
-            </tr>
-            <tr className="border-none">
-              <td className="p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                4985278501
-              </td>
-              <td className="p-2">R$ 30,45</td>
-              <td className="p-2">
-                <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                  Concluído
-                </button>
-              </td>
-            </tr>
-            <tr className="border-none">
-              <td className="p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                4985278501
-              </td>
-              <td className="p-2">R$ 30,45</td>
-              <td className="p-2">
-                <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                  Concluído
-                </button>
-              </td>
-            </tr>
-            <tr className="border-none">
-              <td className="p-2 md-mobile:bg-secondary md-mobile:text-primary">
-                4985278501
-              </td>
-              <td className="p-2">R$ 30,45</td>
-              <td className="p-2">
-                <button className="rounded-3xl bg-secondary text-primary text-sm h-9 w-24 font-semibold">
-                  Concluído
-                </button>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+import UniversalTable from "@/components/Table/UniversalTable";
+import fakeData from "@/components/Table/data/fakedata";
+import { PureComponent } from "react";
+
+export class SalesTable extends PureComponent {
+  state = {
+    currentPage: 1,
+    maxRows: 8,
+  };
+
+  handlePageChange = (pageNumber: number) => {
+    this.setState({ currentPage: pageNumber });
+  };
+  render() {
+    const { currentPage, maxRows } = this.state;
+    const dataToDisplay = fakeData.slice(
+      (currentPage - 1) * maxRows,
+      currentPage * maxRows
+    );
+    const totalPages = Math.ceil(fakeData.length / maxRows);
+
+    return (
+      <div className="flex flex-col items-start max-w-[560px]">
+        <h3 className=" text-base font-semibold ml-2 mb-2 sm-mobile:text-sm">
+          Últimas vendas
+        </h3>
+        <div className="flex sm-mobile:w-[16rem] md-mobile:max-w-[20rem] max-w-[70rem]  sidebar-bp:max-w-[30rem] sm-table:max-w-[22rem] xs-table:max-w-[18rem] overflow-x-auto overflow-y-hidden ">
+          <UniversalTable data={dataToDisplay} compactTable={true} />
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
--- src/components/Sales/Table.tsx
+++ src/components/Sales/Table.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { PureComponent } from "react";
 import { HiOutlinePencil, HiOutlineFilter } from "react-icons/hi";
-import fakeData from "./data/fakedata";
+import fakeData from "../Table/data/fakedata";
 import Pagination from "./components/Pagination";
+import TableData from "../Table/UniversalTable";
 export class Table extends PureComponent {
   state = {
     currentPage: 1,
@@ -10,7 +11,6 @@ export class Table extends PureComponent {
   };
 
   handlePageChange = (pageNumber: number) => {
-    console.log("Página atual:", pageNumber);
     this.setState({ currentPage: pageNumber });
   };
   render() {
@@ -37,52 +37,7 @@ export class Table extends PureComponent {
           <span className="text-base font-semibold mb-2 ">Últimas vendas</span>
         </div>
         <div className="flex">
-          <table className="bg-white text-primary text-left leading-7 inter-font w-full table-fixed rounded-lg">
-            <thead>
-              <tr>
-                <th className="border-b border-primary p-2">ID da venda</th>
-                <th className="border-b border-primary p-2">Valor</th>
-                <th className="border-b border-primary p-2">Data da venda</th>
-                <th className="w-96 border-b border-primary p-2">Descrição</th>
-                <th className="border-b border-primary p-2">Situação</th>
-              </tr>
-            </thead>
-            <tbody>
-              {dataToDisplay.map((item, index) => (
-                <tr
-                  key={item.id}
-                  className={`${
-                    index === dataToDisplay.length - 1
-                      ? "border-t-0 border-b-0"
-                      : "border-b border-primary"
-                  }`}
-                >
-                  <td className="p-2">{item.id}</td>
-                  <td className="p-2">{item.valor}</td>
-                  <td className="p-2">{item.dataVenda}</td>
-                  <td className="p-2">
-                    {item.descricao.length > 40
-                      ? `${item.descricao.substring(0, 40)}...`
-                      : item.descricao}
-                  </td>
-                  <td className="p-2 flex items-center">
-                    <button
-                      className={`rounded-3xl ${
-                        item.situacao === "Pendente"
-                          ? "bg-primary text-white"
-                          : "bg-secondary text-primary"
-                      } text-sm h-9 w-24 font-semibold`}
-                    >
-                      {item.situacao}
-                    </button>
-                    <button className="ml-auto mr-2  text-xl">
-                      <HiOutlinePencil />
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <TableData data={dataToDisplay} compactTable={false} />
         </div>
         <div className="mt-5 flex justify-center">
           <Pagination

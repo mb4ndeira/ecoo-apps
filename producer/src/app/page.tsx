39444e0e8dda821c238534837b58f4155613f973
mb4ndeira <mbandeira.cs@gmail.com>
2023-11-28 19:37:31 -0300

    refactor: bring producer home page to actual project

--- producer/src/app/page.tsx
+++ producer/src/app/page.tsx
@@ -1,5 +1,36 @@
-import { redirect } from "next/navigation";
+import Footer from "@/components/Footer";
+
+import { AccountBalance } from "./home/components/producer/AccountBalance";
+import { Header } from "./home/components/producer/Header/Index";
+import { PendingDeliveries } from "./home/components/producer/PendingDeliveries";
+import { ProductMenu } from "./home/components/producer/ProductMenu";
 
 export default function Home() {
-  redirect("/inicio");
+  const FourItems = 4;
+  const TenItems = 10;
+
+  return (
+    <div className="bg-background">
+      <div className="md:hidden">
+        <Header />
+        <AccountBalance />
+        <ProductMenu />
+        <PendingDeliveries numberOfItems={FourItems} />
+        <Footer backButton={false} />
+      </div>
+      <div className="hidden md:flex flex-col gap-x-16 min-h-96">
+        <div className="w-full flex flex-col justify-between gap-10">
+          <Header />
+        </div>
+        <div className="mt-14 flex gap-x-10">
+          <PendingDeliveries numberOfItems={TenItems} />
+          <div className="flex flex-col justify-between">
+            <AccountBalance />
+            <ProductMenu />
+          </div>
+        </div>
+        <Footer backButton={false} />
+      </div>
+    </div>
+  );
 }

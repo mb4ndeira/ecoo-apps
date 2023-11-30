b710f7251751f870c423515e018865b7dd034e34
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-11-30 15:11:47 -0300

    feat:add dynamic content to these pages

--- cdd/src/app/entregas/[id]/rejeitar/page.tsx
+++ cdd/src/app/entregas/[id]/rejeitar/page.tsx
@@ -2,7 +2,139 @@ import Footer from "@/components/Footer";
 import Link from "next/link";
 import { IoCheckmarkCircle } from "react-icons/io5";
 
-export default function Home() {
+const fakeData = [
+  {
+    id: 123456,
+    nome: "João Fonseca",
+    situacao: "Pendente",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 234567,
+    nome: "Maria Fernandes",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 345678,
+    nome: "Carlos Augusto",
+    situacao: "Rejeitada",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 456789,
+    nome: "Cristiano Ronaldo",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 567890,
+    nome: "Sérgio Gouvea",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 678901,
+    nome: "Tarsila do Amaral",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 789012,
+    nome: "Lionel Messi",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 8910123,
+    nome: "Sandra de Sá",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 901234,
+    nome: "Tyler Herro",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+];
+
+export default function Home({ params }: { params: { id: string } }) {
+  const entregaSelecionada = fakeData.find(
+    (entrega) => entrega.id === parseInt(params.id)
+  );
   return (
     <div className="flex flex-col items-center justify-between min-h-screen bg-background text-slate-gray px-8 md:px-10 lg:px-16 pb-10 pt-10 md:pt-16 lg:pt-20">
       <div className="flex flex-col items-center justify-center flex-grow">
@@ -14,7 +146,8 @@ export default function Home() {
           A entrega foi rejeitada!
         </span>
         <span className="mt-2 text-center text-sm font-medium">
-          A entrega #205004 do produtor José da Silva foi rejeitada.
+          A entrega #{entregaSelecionada?.id} do produtor{" "}
+          {entregaSelecionada?.nome} foi rejeitada.
         </span>
       </div>
       <div className="mt-10 mb-[55px]">

e1973dc3277abc6cc52bbfad272bab9f356d5332
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-12-01 17:46:38 -0300

    feat:integrate the content from these two branches

--- cdd/src/app/entregas/[id]/components/ConfirmationModal.tsx
+++ cdd/src/app/entregas/[id]/components/ConfirmationModal.tsx
@@ -3,23 +3,24 @@ import Modal from "@/components/Modal";
 
 interface ConfirmationModalProps {
   openButton: ReactNode;
+  link: string;
 }
 
 export default function ConfirmationModal({
   openButton,
+  link,
 }: ConfirmationModalProps) {
   return (
-    <div className="flex-1">
-      <Modal
-        openButton={openButton}
-        title="Você tem certeza?"
-        description="Após aprovar a entrega essa operação não poderá ser desfeita. Em
+    <Modal
+      openButton={openButton}
+      title="Você tem certeza?"
+      description="Após aprovar a entrega essa operação não poderá ser desfeita. Em
         caso de erro, entre em contato com o suporte."
-        approvalButtons={true}
-        textButton1="Cancelar"
-        textButton2="Aprovar"
-        bgButton2="#00735E"
-      />
-    </div>
+      approvalButtons={true}
+      textButton1="Cancelar"
+      textButton2="Aprovar"
+      bgButton2="#00735E"
+      link2={link}
+    />
   );
 }
--- cdd/src/app/entregas/[id]/justificativa/page.tsx
+++ cdd/src/app/entregas/[id]/justificativa/page.tsx
@@ -1,6 +1,136 @@
 import Button from "@/components/Button";
 import Dropdown from "@/components/Dropdown";
 import Footer from "@/components/Footer";
+import RejectModal from "./components/RejectModal";
+
+const fakeData = [
+  {
+    id: 123456,
+    nome: "João Fonseca",
+    situacao: "Pendente",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 234567,
+    nome: "Maria Fernandes",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 345678,
+    nome: "Carlos Augusto",
+    situacao: "Rejeitada",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 456789,
+    nome: "Cristiano Ronaldo",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 567890,
+    nome: "Sérgio Gouvea",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 678901,
+    nome: "Tarsila do Amaral",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 789012,
+    nome: "Lionel Messi",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 8910123,
+    nome: "Sandra de Sá",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+  {
+    id: 901234,
+    nome: "Tyler Herro",
+    situacao: "Concluída",
+    prazo: "01/01/2023",
+    conteudo: [
+      "200kg - Cebola Roxa",
+      "40kg - Banana nanica",
+      "140un - Alface crespa",
+      "60kg - Pimentão vermelho",
+      "80kg - Cenoura",
+      "70un - Couve",
+    ],
+  },
+];
 
 const justifications = [
   { id: 1, name: "Falta de produtos", unavailable: false },
@@ -12,7 +142,10 @@ const justifications = [
   { id: 7, name: "Pragas / doenças", unavailable: false },
 ];
 
-export default function Home() {
+export default function Home({ params }: { params: { id: string } }) {
+  const entregaSelecionada = fakeData.find(
+    (entrega) => entrega.id === parseInt(params.id)
+  );
   return (
     <div className="mt-10 flex flex-col bg-background text-slate-gray px-8 md:px-10 lg:px-16 pb-10 pt-10 md:pt-16 lg:pt-20 ">
       <span className="text-center text-3xl font-medium">
@@ -26,9 +159,14 @@ export default function Home() {
         <Dropdown data={justifications} />
       </label>
       <div className="flex fixed bottom-0 left-4 right-4 mb-[85px]">
-        <Button
-          title={"Rejeitar"}
-          className="font-inter py-0 bg-[#FF7070] h-[44px] text-white mt-5 "
+        <RejectModal
+          openButton={
+            <Button
+              title={"Rejeitar"}
+              className="font-inter py-0 bg-[#FF7070] h-[44px] text-white mt-5 "
+            />
+          }
+          link={`/entregas/${entregaSelecionada?.id}/justificativa/rejeitar`}
         />
       </div>
       <Footer backButton={true} />

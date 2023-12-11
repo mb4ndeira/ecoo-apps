d9b6641fd3960111ec76e5c4e59568b63236f82f
ecorrea70 <eduardodasilvacorrea70@gmail.com>
2023-12-11 11:34:29 -0300

    feat: add some custom colors to tailwind config file

--- cdd/tailwind.config.ts
+++ cdd/tailwind.config.ts
@@ -9,9 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
+        default: "#3E5155",
         primary: "#545F71",
         secondary: "#eef1f4",
         background: "#F7F7F7",
+        reject: "#FF7070",
         "slate-blue": "#507080",
         "slate-gray": "#2F4A4D",
         "rain-forest": "#00735E",

49cf361d2a16beefb32f8bdeb2ed2da7b4dc938f
mb4ndeira <mbandeira.cs@gmail.com>
2024-02-24 16:51:17 -0300

    feat: restrain destkop visualization

--- cdd/src/components/Footer.tsx
+++ cdd/src/components/Footer.tsx
@@ -5,14 +5,13 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="bottom-0 fixed justify-between w-full flex pb-5 right-0 px-5 z-99 bg-white">
+    <div className="fixed bottom-0 left-0 right-0 flex justify-between max-w-md mx-auto bg-white p-5">
       <Link href={"/"} className="flex items-center">
         <LuChevronLeft className="w-[30px] h-[30px] text-default" />
-        <Button 
+        <Button
           title="Voltar"
           className="flex items-center gap-2 text-sm font-medium text-default w-auto"
-        >
-        </Button>
+        ></Button>
       </Link>
       <div>
         <IoIosHelp className="w-[50px] h-[50px] rounded-full border-0 text-white bg-default" />
--- producer/src/components/Footer.tsx
+++ producer/src/components/Footer.tsx
@@ -18,17 +18,16 @@ export default function Footer({ backButton }: FooterProps) {
   };
 
   return (
-    <div className="bottom-0 fixed justify-between w-full flex pb-5 right-0 px-5 z-99">
+    <div className="fixed bottom-0 left-0 right-0 flex justify-between max-w-md mx-auto bg-white p-5">
       {backButton && (
         <div className="bottom-0 fixed justify-between w-full flex pb-5 right-0 px-5 bg-white">
           <Link href={"/"} className="flex items-center">
             <LuChevronLeft className="w-[30px] h-[30px] text-default" />
-            <Button 
+            <Button
               title="Voltar"
               className="flex items-center gap-2 text-sm font-medium text-default w-auto"
               onClick={handleGoBack}
-            >
-            </Button>
+            ></Button>
           </Link>
           <div className="w-full flex justify-end">
             <IoIosHelp className="w-[50px] h-[50px] rounded-full border-0 text-white bg-default" />

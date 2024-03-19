314d97bbb2458524beafc4defb5d49fbf1698eee
mb4ndeira <mbandeira.cs@gmail.com>
2024-03-18 22:11:24 -0300

    refactor: expose stub store on use cases

--- domain/use-cases/register-agribusiness.ts
+++ domain/use-cases/register-agribusiness.ts
@@ -13,7 +13,7 @@ interface RegisterAgribusinessData {
 export const registerAgribusiness: UseCaseHandler<
   RegisterAgribusinessData,
   Promise<{ agribusiness: Agribusiness }>
-> = async (data, _stubbed, { setOrStub }, axios) => {
+> = async (data, stubbed, { store }, axios) => {
   const agribusiness = Agribusiness.create({
     name: "Fazenda Teixeira",
     caf: "223989203092",
@@ -26,23 +26,22 @@ export const registerAgribusiness: UseCaseHandler<
     })
   ).data;
 
-  await setOrStub({
-    real: async () => {
-      await axios.post(
-        `${process.env.API_URL}/agribusinesses`,
-        {
-          caf: data.caf,
-          name: data.name,
+  if (!stubbed) {
+    await axios.post(
+      `${process.env.API_URL}/agribusinesses`,
+      {
+        caf: data.caf,
+        name: data.name,
+      },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
         },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-    },
-    stub: ["agribusiness", agribusiness],
-  });
+      }
+    );
+  } else {
+    store("agribusiness", agribusiness);
+  }
 
   return {
     agribusiness,

fc5aecde66b47ff5a3dc762da6f30fd38edb6294
eduardo.correa <eduardo.correa03@edu.pucrs.br>
2024-04-29 19:37:01 -0300

    feat: change in the way pagination pages are displayed

--- producer/src/app/(footered)/produtos/vender/components/RenderProducts.tsx
+++ producer/src/app/(footered)/produtos/vender/components/RenderProducts.tsx
@@ -1,5 +1,4 @@
-'use client'
-
+"use client";
 import { GetProducts } from "@producer/app/_actions/products/GetProducts";
 import Image, { ImageLoader } from "next/image";
 import Link from "next/link";
@@ -11,50 +10,50 @@ import { IoIosArrowForward } from "react-icons/io";
 import { LuChevronLeft } from "react-icons/lu";
 
 interface Products {
-  id: string
-  name: string
-  image: string
-  pricing: string
+  id: string;
+  name: string;
+  image: string;
+  pricing: string;
 }
 
-export default function RenderProducts(){
-  const [page, setPage] = useState<number>(1)
-  const inputRef = useRef<HTMLInputElement>(null)
-  const [query, setQuery] = useState<string>("")
-  const [products, setProducts] = useState<[Products] | []>()
-  const router = useRouter()
+export default function RenderProducts() {
+  const [page, setPage] = useState<number>(1);
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [query, setQuery] = useState<string>("");
+  const [products, setProducts] = useState<[Products] | []>();
+  const router = useRouter();
 
   const handleChangeInput = async (e: ChangeEvent<HTMLInputElement>) => {
     setTimeout(async () => {
-      setQuery(inputRef.current?.value as string) 
-    }, 200)
-  }
+      setQuery(inputRef.current?.value as string);
+    }, 200);
+  };
 
   const backPage = () => {
-    if(page > 1){
-      setPage(prev => prev - 1)
+    if (page > 1) {
+      setPage((prev) => prev - 1);
     }
-  }
+  };
 
   const nextPage = () => {
-    if(page < 8){
-      setPage(prev => prev + 1)
+    if (page < 8) {
+      setPage((prev) => prev + 1);
     }
-  }
+  };
 
   useEffect(() => {
     (async () => {
-      if(query !== ""){
-        const response = await GetProducts({ name: query, page: 1 })
-        setProducts(response?.data)
-        return
+      if (query !== "") {
+        const response = await GetProducts({ name: query, page: 1 });
+        setProducts(response?.data);
+        return;
       }
 
-      const response = await GetProducts({ name: query, page: page })
+      const response = await GetProducts({ name: query, page: page });
 
-      setProducts(response?.data)
-    })()
-  }, [query, page])
+      setProducts(response?.data);
+    })();
+  }, [query, page]);
 
   const handleSelectProduct = (id: string, name: string, pricing: string) => {
     const dataOnStorage = JSON.parse(
@@ -67,18 +66,18 @@ export default function RenderProducts(){
         ...dataOnStorage,
         name: name,
         id: id,
-        pricing: pricing
+        pricing: pricing,
       })
     );
-    
-    router.push('/produtos/vender/adicionar')
-  }
+
+    router.push("/produtos/vender/adicionar");
+  };
 
   const imageLoader: ImageLoader = ({ src }) => {
     return `https://res.cloudinary.com/dwm7zdljf/image/upload/v1706539060/products-images/256x256_${src}`;
-  }
+  };
 
-  return(
+  return (
     <div className="w-full">
       <div className="relative mt-8 w-full">
         <form>
@@ -98,15 +97,22 @@ export default function RenderProducts(){
       </div>
       <div className="w-full h-full mb-5 flex flex-col justify-between">
         <div className="grid grid-cols-2 justify-items-start gap-3 w-full mt-4 p-4">
-          {products && products.length !== 0 ? products.map((product, index) => {
-            return (
-              <button
-                className="flex flex-col items-center rounded-2xl w-full h-[160px] p-2.5 bg-white"
-                key={index}
-                onClick={() => handleSelectProduct(product.id, product.name, product.pricing)}
-              >
-                <div className="relative w-full min-w-[130px] h-[100px] rounded-[10px]">
-                    <Image
+          {products && products.length !== 0
+            ? products.map((product, index) => {
+                return (
+                  <button
+                    className="flex flex-col items-center rounded-2xl w-full h-[160px] p-2.5 bg-white"
+                    key={index}
+                    onClick={() =>
+                      handleSelectProduct(
+                        product.id,
+                        product.name,
+                        product.pricing
+                      )
+                    }
+                  >
+                    <div className="relative w-full min-w-[130px] h-[100px] rounded-[10px]">
+                      <Image
                         className="rounded-[10px]"
                         loader={imageLoader}
                         src={product.image}
@@ -114,17 +120,18 @@ export default function RenderProducts(){
                         quality={256}
                         layout="fill"
                         objectFit="cover"
-                    />
-                </div>
-                <span className="pt-2.5 text-base leading-[22px] tracking-tight text-slate-gray">
-                    {product.name}
-                </span>
-            </button>
-            );
-          }) : null}
+                      />
+                    </div>
+                    <span className="pt-2.5 text-base leading-[22px] tracking-tight text-slate-gray">
+                      {product.name}
+                    </span>
+                  </button>
+                );
+              })
+            : null}
         </div>
-        {(!products || products.length === 0) ? (
-          <div className="w-full flex justify-center right-2 items-center fixed bottom-16 gap-4 text-lg text-slate-gay mt-2">
+        {!products || products.length === 0 ? (
+          <div className="w-full flex justify-center right-2 items-center fixed bottom-16 gap-4 text-lg text-slate-gray mt-2">
             <button onClick={backPage}>
               <IoIosArrowBack />
             </button>
@@ -134,33 +141,38 @@ export default function RenderProducts(){
             </button>
           </div>
         ) : (
-          <div className="w-full flex justify-center gap-4 items-center text-lg text-slate-gay mt-2">
+          <div className="w-full flex justify-center gap-4 items-center text-lg text-slate-gray mt-2">
             <button onClick={backPage}>
               <IoIosArrowBack />
             </button>
-            {page}
+            {page} de {Math.ceil(products.length / 8)}
             <button onClick={nextPage}>
               <IoIosArrowForward />
             </button>
           </div>
         )}
-        
       </div>
-      {(!products || products.length === 0) ? (
+      {!products || products.length === 0 ? (
         <div className="flex items-center mt-2 fixed bottom-4">
-        <LuChevronLeft className="w-[30px] h-[30px] text-default" />
-        <Link className="flex items-center gap-2 text-sm font-medium text-default w-auto" href={"/produtos/vender/ciclo"}>
-          Voltar
-        </Link>
-      </div>
+          <LuChevronLeft className="w-[30px] h-[30px] text-default" />
+          <Link
+            className="flex items-center gap-2 text-sm font-medium text-default w-auto"
+            href={"/produtos/vender/ciclo"}
+          >
+            Voltar
+          </Link>
+        </div>
       ) : (
         <div className="flex items-center mt-2">
-        <LuChevronLeft className="w-[30px] h-[30px] text-default" />
-        <Link className="flex items-center gap-2 text-sm font-medium text-default w-auto" href={"/produtos/vender/ciclo"}>
-          Voltar
-        </Link>
-      </div>
+          <LuChevronLeft className="w-[30px] h-[30px] text-default" />
+          <Link
+            className="flex items-center gap-2 text-sm font-medium text-default w-auto"
+            href={"/produtos/vender/ciclo"}
+          >
+            Voltar
+          </Link>
+        </div>
       )}
     </div>
-  )
+  );
 }

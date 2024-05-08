2a4ec55324d8fcccfbacc6cdc3d6bc1903a3a8cb
gabrieltjaeger <gabrieltjaeger@gmail.com>
2024-05-08 02:07:37 -0300

    Fix: corrected user name fetching through use-case

--- cdd/src/app/_actions/get-profile.ts
+++ cdd/src/app/_actions/get-profile.ts
@@ -1,26 +1,21 @@
 "use server";
 
-import axios from "axios";
 import { cookies } from "next/headers";
+import { USE_CASES } from "@shared/domain/use-cases";
 
 export async function getProfile() {
   const token = cookies().get("token")?.value as string;
 
-  if(token){
-    try{
-      const config = {
-        headers: { Authorization: `Bearer ${token}` },
-      };
-    
-      const data = await axios.get(
-        `${process.env.API_URL}/me`,
-        config
-      );
-    
-      return data.data;
-    }catch (error) {
+  if (token) {
+    try {
+      const user = await (
+        await USE_CASES["get-user"].execute({ access_token: token })
+      ).data;
+      return user.me;
+    } catch (error) {
       console.error("Erro ao fazer a chamada à API:", error);
     }
+  } else {
+    throw new Error("Token não encontrado");
   }
-
 }

7d65326f300d839d06fe270b0ddef137f5fd62ec
Eduardo Corrêa <eduardodasilvacorrea70@gmail.com>
2024-07-23 00:31:10 -0300

    refactor: update variable names in getCycles use case

--- producer/src/app/(footered)/home/components/CycloInformation.tsx
+++ producer/src/app/(footered)/home/components/CycloInformation.tsx
@@ -8,44 +8,42 @@ import { HiOutlineInformationCircle } from "react-icons/hi";
 import { toast } from "sonner";
 
 export default function CycloInformation() {
-  const { cycle } = useCycleProvider()
+  const { cycle } = useCycleProvider();
   const [message, setMessage] = useState<string>("");
-  const router = useRouter()
+  const router = useRouter();
 
   const handleClickButton = () => {
-    if(!cycle){
-        toast.warning("Selecione um ciclo para ver mais informações sobre ele!")
-        return
+    if (!cycle) {
+      toast.warning("Selecione um ciclo para ver mais informações sobre ele!");
+      return;
     }
 
-    router.push("/informacoes-ciclo")
-  }
+    router.push("/informacoes-ciclo");
+  };
 
   useEffect(() => {
-    if(cycle !== undefined){
+    if (cycle !== undefined) {
       const diaAtual = new Date().getDay() + 1;
 
-      const { offering, ordering, dispatching } = cycle
+      const { offer, order, deliver } = cycle;
 
-      if (Array.isArray(offering) && offering.includes(diaAtual)) {
+      if (Array.isArray(offer) && offer.includes(diaAtual)) {
         setMessage(" fazer a sua oferta");
-      } else if (Array.isArray(ordering) && ordering.includes(diaAtual)) {
+      } else if (Array.isArray(order) && order.includes(diaAtual)) {
         setMessage(" dia de compras");
-      } else if (
-        Array.isArray(dispatching) &&
-        dispatching.includes(diaAtual)
-      ) {
+      } else if (Array.isArray(deliver) && deliver.includes(diaAtual)) {
         setMessage(" entregar ao CDD");
       }
     }
-  }, [cycle])
+  }, [cycle]);
 
   return (
     <div className="mt-5 w-full h-fit py-5 rounded-2xl bg-white p-4">
       <div className="flex justify-between items-center">
         {cycle ? (
           <p className="text-default text-[16px]">
-            É hora de: <span className="text-[#00735E] font-bold">{message}</span>
+            É hora de:{" "}
+            <span className="text-[#00735E] font-bold">{message}</span>
           </p>
         ) : (
           <p className="text-theme-primary text-[16px]">
